4e8c76a29153c9c451cb75e07c37473afe25bbb3
강서현 <gangseohyeon@TAG27.local>
2020-01-07 17:59:23 +0900

    Solidity Complete

--- contract/edu_token.sol
+++ contract/edu_token.sol
@@ -3,30 +3,30 @@ pragma solidity ^0.4.18;
 contract Token {
 
     /// @return total amount of tokens
-    function totalSupply() public constant returns (uint256 supply) {}
+    //function totalSupply() public constant returns (uint256 supply) {}
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance) {}
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @notice send _value token to _to from msg.sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) returns (bool success) {}
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @notice send _value token to _to from _from on the condition it is approved by _from
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {} 
 
-    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @notice msg.sender approves _addr to spend _value tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) returns (bool success) {}
+    //function approve(address _spender, uint256 _value) returns (bool success) {}
 
     /// @param _owner The address of the accountƒ√ owning tokens
     /// @param _spender The address of the account able to transfer the tokens
@@ -86,15 +86,15 @@ contract StandardToken is Token {
     }
 
     function getMiddleMoney(address _owner, address _spender) constant returns (uint256 remaining) {
-      return middleMoney[_owner][_spender];
+        return middleMoney[_owner][_spender];
     }
 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) middleMoney;
-    uint256 public a;
+
 }
 
-contract SuperUltraPowerCoin is StandardToken { // CHANGE THIS. Update the contract name.
+contract EduCoin is StandardToken { // CHANGE THIS. Update the contract name.
 
     /* Public variables of the token */
 
@@ -114,12 +114,12 @@ contract SuperUltraPowerCoin is StandardToken { // CHANGE THIS. Update the contr
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function SuperUltraPowerCoin() {
+    function EduCoin() {
         balances[msg.sender] = 100000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         //totalSupply = 1000000000000000000000;                                // Update total supply (1000 for example) (CHANGE THIS)
-        name = "SuperUltraPowerCoin";                                   // Set the name for display purposes (CHANGE THIS)
+        name = "EduCoin";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "SUPC";                                             // Set the symbol for display purposes (CHANGE THIS)
+        symbol = "edu";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 10000;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
@@ -149,4 +149,4 @@ contract SuperUltraPowerCoin is StandardToken { // CHANGE THIS. Update the contr
     //     if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
     //     return true;
     // }
-} 
+}
