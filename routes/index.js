7ad38c9e854a30f7ebb004f5a74581b5b837b1f2
강서현 <gangseohyeon@TAG27.local>
2020-01-07 20:51:38 +0900

    20200107)2051 - Mentor List Paging Complete

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,37 @@ var indexRouter = express.Router();
 var mentorModel = require('../model/mentorModel');
 var mentiModel = require('../model/mentiModel');
 
-indexRouter.get('/api/introduction', async (req, res) => {
+indexRouter.get('/api/introduction/:cur', async (req, res) => {
     try {
         var returnData = await mentorModel.getAllMentor();
         console.log(returnData);
-        res.status(200).send(returnData);
+
+        //몇 명의 멘토를 출력할 것인가?
+        var page_list_size = 9;
+        var totalCount = 0;
+
+        //멘토 수
+        var totalCount = returnData;
+
+        //현재 페이지
+        var curPage = req.params.cur;
+
+        if (totalCount < 0) {
+            totalCount = 0;
+        }
+        var pagingResult = {
+            'totalCount': totalCount,
+            'curPage': curPage,
+            'page_list_size': page_list_size,
+        };
+        res.status(200).send(pagingResult);
     } catch (err) {
         console.log(err);
         res.status(500).send(err);
     }
 });
 
-indexRouter.post('/api/introduction:/id', async (req, res) => {
+indexRouter.post('/api/introduction/:id', async (req, res) => {
     try {
         console.log(req.params);
         var mentorCode = req.params.id;
